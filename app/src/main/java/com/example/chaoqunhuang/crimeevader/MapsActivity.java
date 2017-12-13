d448c86263b2aca43159214e891b40ebeddeb3b3
Jerry <chaoqunhuang@ChaoqunsMacBook.home>
2017-12-13 12:56:17 -0500

    change style to dark

--- app/src/main/java/com/example/chaoqunhuang/crimeevader/MapsActivity.java
+++ app/src/main/java/com/example/chaoqunhuang/crimeevader/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.chaoqunhuang.crimeevader;
 
+import android.content.res.Resources;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,7 +39,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
 
+        } catch (Resources.NotFoundException e) {
+            System.out.println(e.getMessage());
+        }
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
